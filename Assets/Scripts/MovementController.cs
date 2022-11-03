ab7cc2ed536fa3526d69a86f337f6459ecbfa32d
Bobbossa <bobbossa@gmail.com>
2022-11-03 22:26:24 +0000

    Vehicle now rotates, but this does not change it's forward or backward direction.

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,7 +7,7 @@ public class MovementController : MonoBehaviour
 {
     //Speed multipliers values editable in unity editor.
     public float accelerationVar = 1;
-    public float yawSpeedVar = 1;
+    public float yawSpeedVar = 100;
     //public float rollSpeedVar = 1;
     //public float maxSpeed = 10;
     //public float minSpeed = -10;
@@ -17,24 +17,43 @@ public class MovementController : MonoBehaviour
     float yawSpeedMult = 0;
     //float rollSpeedMult = 0;
     float acceleration = 0;
+    float yawSpeed = 0;
+    Vector3 yawAngularVelocity = new Vector3(0, 0, 0);
+    Rigidbody vehicleRB;
+
     //Add strafe?
 
     //GetComponent<Rigidbody>().AddForce(Vector3.forward * variable, ForceMode.Acceleration);
 
+    private void Start()
+    {
+        vehicleRB = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        accelerationMult = acceleratorInputCheck();
+        accelerationMult = AcceleratorInputCheck();
+        yawSpeedMult = YawInputCheck();
     }
 
     void FixedUpdate()
     {
-        acceleration = accelerationSet(accelerationMult, accelerationVar);
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * acceleration, ForceMode.Acceleration);
+        acceleration = SpeedSet(accelerationMult, accelerationVar);
+        vehicleRB.AddForce(Vector3.forward * acceleration, ForceMode.Acceleration);
+
+        //Yaw stuff still needs to be cleaned up, sorry.
+        yawSpeed = SpeedSet(yawSpeedMult, yawSpeedVar);
+        yawAngularVelocity = Vector3.up * yawSpeed;
+        
+        
+        Quaternion deltaRotation = Quaternion.Euler(yawAngularVelocity * Time.fixedDeltaTime);
+        vehicleRB.MoveRotation(vehicleRB.rotation * deltaRotation);
+
     }
 
-    
 
-    float acceleratorInputCheck()
+
+    float AcceleratorInputCheck()
     {
         float accelerationMult = 0;
         //Checks for forward or backwards input.
@@ -50,15 +69,29 @@ public class MovementController : MonoBehaviour
 
         return accelerationMult;
     }
-    //void yawInputCheck()
-    //{
 
-    //}
-    float accelerationSet(float accelerationMult, float accelerationVar)
+    float YawInputCheck()
+    {
+        float yawMult = 0;
+        //Checks for left or right input.
+        if (Input.GetKey(KeyCode.D))
+        {
+            yawMult = yawSpeedMult + 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            yawMult = yawSpeedMult - 1;
+        }
+
+        return yawMult;
+    }
+
+    float SpeedSet(float multiplier, float variable)
     {
         float acceleration = 0;
 
-        acceleration = accelerationMult * accelerationVar;
+        acceleration = multiplier * variable;
         Debug.Log(acceleration);
 
         return acceleration;
