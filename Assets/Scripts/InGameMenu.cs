801d876ad0a59a3f046c5f9c3d9c4eb60d3d04c8
Andrew Smith <thranriel@gmail.com>
2022-11-04 17:01:20 +0000

    Add in game menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    GameObject child;
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Start() {
+        child = transform.GetChild(0).gameObject;
+    }
+
+    void Update() {
+        if (Input.GetKeyUp("escape")) {
+            child.active = !child.active;
+        }
+    }
+}
