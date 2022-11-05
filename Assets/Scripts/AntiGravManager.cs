8b1e9f9cd8528ed7c1806547720c6fa38488feb9
Bobbossa <bobbossa@gmail.com>
2022-11-05 23:05:08 +0000

    Hover is now smoooooooooooooth. Feel free to use gravity and unlock any y position locks.

--- Assets/Scripts/AntiGravManager.cs
+++ Assets/Scripts/AntiGravManager.cs
@@ -10,7 +10,10 @@ public class AntiGravManager : MonoBehaviour
     public Vector3 hoverRayYOffset = new Vector3(0.0f, -1.0f, 0.0f);
     public float hoverRayDistance = 1.0f;
 
-    public float hoverForce = 280f;
+    public float hoverForce = 26.8f;
+    public float hoverConstant = 2.5f;
+    float hoverRegulator;
+    float distanceToFloor;
 
     RaycastHit hitInfo;
     LayerMask hoverMask;
@@ -19,6 +22,8 @@ public class AntiGravManager : MonoBehaviour
     Vector3 hoverRay1XZOffset = new Vector3(0.0f, 0.0f, 0.0f);
     Vector3 hoverRay1CombOffset;
 
+
+
     void Start()
     {
         vehicleRB = GetComponent <Rigidbody>();
@@ -26,23 +31,21 @@ public class AntiGravManager : MonoBehaviour
         hoverMask = LayerMask.GetMask("Ship");
         hoverRay1CombOffset = OffsetCombiner(hoverRay1XZOffset);
 
-
-
-
     }
 
     private void FixedUpdate()
     {
-        hoverRay1 = new Ray(transform.localPosition , transform.up * -1);
-        Debug.DrawRay(transform.localPosition, transform.up * 10 * -1, Color.red, 9999, true);
-        //Debug.DrawRay(vehicleCollider.transform.localPosition + offset, vehicleCollider.transform.forward, Color.red, 9999999, true); ;
+        hoverRay1 = new Ray(transform.localPosition, transform.up * -1);
+        Debug.DrawRay(transform.localPosition, transform.up * 10 * -1, Color.yellow, 1, true);
+        //Debug.DrawRay(vehicleCollider.transform.localPosition + offset, vehicleCollider.transform.forward, Color.red, 9999999, true);        
 
         if (GroundDetector(hoverRay1))
         {
-            vehicleRB.AddRelativeForce(Vector3.up * hoverForce, ForceMode.Force);
+            vehicleRB.AddRelativeForce(Vector3.up * HoverSmoother(hoverRay1), ForceMode.Force);
         }
+        
     }
-
+    
     //Add more inputs and GroundDetector() calls as neccesary.
     void DetectorManager(Ray inputRay1)
     {
@@ -56,13 +59,14 @@ public class AntiGravManager : MonoBehaviour
         if (Physics.Raycast(inputRay, out hitInfo, hoverRayDistance))
         {
             groundDetected = true;
-            Debug.Log(hitInfo.collider + " detected by GroundDetector() in AntiGravManager");
+           // Debug.Log(hitInfo.collider + " detected by GroundDetector() in AntiGravManager");
+           // Debug.Log(hitInfo.distance);
         }
 
         if (Physics.Raycast(inputRay, out hitInfo, hoverRayDistance, hoverMask))
         {
             groundDetected = false;
-            Debug.Log("Ship detected by GroundDetector() in AntiGravManager");
+           // Debug.Log("Ship detected by GroundDetector() in AntiGravManager");
         }
 
         return groundDetected;
@@ -77,5 +81,18 @@ public class AntiGravManager : MonoBehaviour
         return combinedOffset;
     }
 
+    float HoverSmoother(Ray inputRay)
+    {
+        float newHoverForce = 0.0f;
+
+        Physics.Raycast(inputRay, out hitInfo, hoverRayDistance);
+        distanceToFloor = hitInfo.distance;
+        //hoverRegulator increases as distance to the floor lowers. Can be adjusted by changing hoverConstant.
+        hoverRegulator = (hoverConstant / (distanceToFloor + 1)); 
+        newHoverForce = hoverForce * hoverRegulator;
+
+        Debug.Log("From HoverSmoother in AntiGravManager. newHoverForce = " + newHoverForce);
+        return newHoverForce;
+    }
 
 }
